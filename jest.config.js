c2755e9e83f3a45b7e77a647aa1daa53f80a3a08
Mustafa Mansour <33766083+MkMan@users.noreply.github.com>
2022-06-25 08:05:06 +1000

    chore: add jest-watch-typeahead

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
   coveragePathIgnorePatterns: ['index.*'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 };
